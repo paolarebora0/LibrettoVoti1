a05229617f00ee392a5ed9341c43f0907e53b0db
Paola <Paola@PC-PaolaLaMagnifica>
2020-01-06 11:43:04 +0100

    Metodi standard aggiunti

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,11 +1,37 @@
 package it.polito.tdp.libretto;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 
 public class Voto {
 	private int voto;
 	private String corsoString;
-	private Calendar data;
+	private LocalDate data;
+	
+	public Voto(int voto, String corsoString, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corsoString = corsoString;
+		this.data = data;
+	}
+	
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorsoString() {
+		return corsoString;
+	}
+	public void setCorsoString(String corsoString) {
+		this.corsoString = corsoString;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 	
 	
 
