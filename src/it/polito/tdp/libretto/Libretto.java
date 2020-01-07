9bb55c52088e401ec7c9d922691d5acf2f686568
Paola <Paola@PC-PaolaLaMagnifica>
2020-01-07 15:58:07 +0100

    Finito punto 1.

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,33 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti = new ArrayList<Voto>();
+	}
+	
+	
+//	public void add(int voto, String corso, LocalDate data) {
+//		
+//	}
+	
+//	Non ho bisogno di sapere come è fatto il voto per aggiungerlo al libretto
+	/**
+	 * Aggiunge un nuovo voto al libretto
+	 * 
+	 * @param v il {@link Voto} da aggiungere
+	 */
+	public void add(Voto v) {
+		voti.add(v);
+		//questo metodo delega alla lista sottostante di aggiungere il voto
+	}
+	
+	
 
 }
